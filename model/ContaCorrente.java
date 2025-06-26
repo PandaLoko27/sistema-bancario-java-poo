937aa3a64e05037052a1cbff0fd5d6f9bfa5ca09
Otavio2007 <otavioaredes62@gmail.com>
2025-06-26 20:03:16 -0300

    Classe da conta corrente

---
+++ model/ContaCorrente.java
@@ -0,0 +1,15 @@
+public class ContaCorrente extends Conta {
+    public ContaCorrente(Cliente cliente, String numeroConta) {
+        super(cliente, numeroConta);
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            historico.add("Saque: R$" + valor);
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+}
